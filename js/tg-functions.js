41bd2935c1b6e5db5ffedfbfc4063eae577b0208
Inspector Soviet <www.alex.avil@mail.ru>
2022-04-26 19:45:17 +0400

    main button test

--- js/tg-functions.js
+++
@@ -1,9 +0,0 @@
-Telegram.WebApp.MainButton.setParams({
-    is_visible: true,
-    text: "Get Started"
-}).onClick(this.mainBtnClicked);
-
-mainBtnClicked = function() {
-    Telegram.WebApp.sendData("/setwebsite");
-    console.log(Telegram.WebApp.sendData("/setwebsite"));
-}
